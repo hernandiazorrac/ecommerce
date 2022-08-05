30f8446866efcd5c283edde289bdf5657c6b3e87
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2022-08-04 23:02:43 -0300

    enlace emptycart a tienda arreglado

--- src/components/EmptyCart/EmptyCart.js
+++ src/components/EmptyCart/EmptyCart.js
@@ -6,7 +6,7 @@ export const EmptyCart = () => {
         <>
             <img className="emptyCartImg img-fluid" alt='carrito vacío' draggable="false" src='https://res.cloudinary.com/digb4d0uf/image/upload/c_scale,w_800/v1659573559/ReactJS%20Coder/empty-cart_esspy1.webp' />
             <p className='emptyCartText text-muted'>No hay productos en el carrito.</p>
-            <p className='emptyCartText'><Link to="/ecommerce">Volver a la tienda</Link></p>
+            <p className='emptyCartText'><Link to="/">Volver a la tienda</Link></p>
        </>
     )
 }
